63a54b23062bd8e641f15ac7b9f5d75b44e02dab
Mike Carouchi <mikecarouchi@gmail.com>
2022-01-18 12:14:00 -0800

    test fix 4

--- app.py
+++ app.py
@@ -15,11 +15,12 @@ from flask_cors import CORS, cross_origin
 from werkzeug.utils import redirect
 
 
-# CORS(app,resources={r"/api/*":{"origins":"*"}})app = Flask(__name__)
+
 
 
 # app.config['CORS HEADERS'] = 'Content-Type'
 app = Flask(__name__)
+CORS(app,resources={r"/api/*":{"origins":"*"}}) 
 
 basedir = os.path.abspath(os.path.dirname(__file__))
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///' + os.path.join(basedir, 'app.sqlite')
