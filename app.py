79be43c06c9be32846f16848bf85114626d65246
Mike Carouchi <mikecarouchi@gmail.com>
2022-01-20 05:17:09 -0800

    test

--- app.py
+++ app.py
@@ -144,8 +144,8 @@ def login():
     if not user and not check_password_hash(user.password, password):
         flash('Check info and try again!')
 
-        login_user(user)
-        return jsonify(User.dump(user))
+    login_user(user)
+    return jsonify(User.dump(user))
         # return redirect('/') #return value rather than redirect jsonify
 
     return redirect('/blogs')
